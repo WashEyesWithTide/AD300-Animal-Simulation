1837989685d99551abb25579c9ae05d2470a24c6
Robert Sun <145725240+WashEyesWithTide@users.noreply.github.com>
2026-01-18 15:52:37 -0800

    added sponsor

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,19 @@
 package src;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         ArrayList<Animal> animals = new ArrayList<>();
+
+        System.out.print("Please enter the password: ");
+        if (!scanner.nextLine().equals("ROBERTSUN50")) {
+            System.out.println("Incorrect password. Access denied.");
+            scanner.close();
+            System.exit(0);
+        }
 
         animals.add(new Dog("Buddy", 3));
         animals.add(new Cat("Tom", 2));
